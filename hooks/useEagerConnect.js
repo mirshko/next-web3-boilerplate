74a2cd67c3eebc9f606266a6920a5fb49fdbdb43
Jeff Reiner <gone@hey.com>
2020-07-20 12:26:09 +0200

    Fix broken useEagerConnect

--- hooks/useEagerConnect.js
+++ hooks/useEagerConnect.js
@@ -1,5 +1,6 @@
 import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
+import { injected } from "../connectors";
 
 export default function useEagerConnect() {
   const { activate, active } = useWeb3React();
