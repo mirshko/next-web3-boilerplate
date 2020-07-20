b08c02bffc7834fdb7a7bc0619807833eea14058
Jeff Reiner <gone@hey.com>
2020-07-20 12:26:24 +0200

    Adding utils

---
+++ util.js
@@ -0,0 +1,31 @@
+export function shortenHex(hex, length = 4) {
+  return `${hex.substring(0, length + 2)}…${hex.substring(
+    hex.length - length
+  )}`;
+}
+
+const ETHERSCAN_PREFIXES = {
+  1: "",
+  3: "ropsten.",
+  4: "rinkeby.",
+  5: "goerli.",
+  42: "kovan.",
+};
+
+/**
+ *
+ * @param {("Account"|"Transaction")} type
+ * @param {[number, string]} data
+ */
+export function formatEtherscanLink(type, data) {
+  switch (type) {
+    case "Account": {
+      const [chainId, address] = data;
+      return `https://${ETHERSCAN_PREFIXES[chainId]}etherscan.io/address/${address}`;
+    }
+    case "Transaction": {
+      const [chainId, hash] = data;
+      return `https://${ETHERSCAN_PREFIXES[chainId]}etherscan.io/tx/${hash}`;
+    }
+  }
+}
