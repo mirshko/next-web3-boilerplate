1c6666e1804431c47c9bd0595ad1d671cc0d76c9
Jeff Reiner <gone@hey.com>
2020-07-20 12:31:46 +0200

    Dropping in Account and using useEagerConnect to autoconnect

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Head from "next/head";
+import Account from "../components/Account";
+import useEagerConnect from "../hooks/useEagerConnect";
 
 export default function Home() {
+  const triedToEagerConnect = useEagerConnect();
+
   return (
     <div>
       <Head>
         <title>Next Web3 Boilerplate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <header>
+        <nav>
+          <Account triedToEagerConnect={triedToEagerConnect} />
+        </nav>
+      </header>
 
       <main>
         <h1>
