e459b112b39e2171aa3f7a1cf9380b231b872df8
Jeff Reiner <gone@hey.com>
2020-07-20 13:05:52 +0200

    Rename address to tokenAddress for more specificity

--- hooks/useERC20Contract.js
+++ hooks/useERC20Contract.js
@@ -9,8 +9,8 @@ const ABI = [
 /**
  * @name useERC20Contract
  * @description Uses the new Human-Readable ABI format from ethers v5. Supports ERC20 contract functions of 'balanceOf', 'transfer', and the 'Transfer' event itself.
- * @param {string} address
+ * @param {string} tokenAddress
  */
-export default function useERC20Contract(address) {
-  return useContract(address, ABI);
+export default function useERC20Contract(tokenAddress) {
+  return useContract(tokenAddress, ABI);
 }
