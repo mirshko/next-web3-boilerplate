9711ee65f458bdb9b693fd0983b5e4999d170a7e
Jeff Reiner <gone@hey.com>
2020-09-22 19:16:03 +0200

    Cleaning up hooks

--- hooks/usePersonalSign.js
+++ hooks/usePersonalSign.js
@@ -1,7 +1,12 @@
 import { useWeb3React } from "@web3-react/core";
 
+export const hexlify = (message) =>
+  "0x" + Buffer.from(message, "utf8").toString("hex");
+
 export default function usePersonalSign() {
   const { library, account } = useWeb3React();
 
-  return async (message) => library.send("personal_sign", [message, account]);
+  return async (message) => {
+    return library.send("personal_sign", [hexlify(message), account]);
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Account from "../components/Account";
 import ETHBalance from "../components/ETHBalance";
 import useEagerConnect from "../hooks/useEagerConnect";
-import usePersonalSign from "../hooks/usePersonalSign";
+import usePersonalSign, { hexlify } from "../hooks/usePersonalSign";
 
 export default function Home() {
   const { account, library } = useWeb3React();
@@ -17,6 +17,7 @@ export default function Home() {
   const handleSign = async () => {
     const msg = "Next Web3 Boilerplate Rules";
     const sig = await sign(msg);
+    console.log(sig);
     console.log("isValid", verifyMessage(msg, sig) === account);
   };
 
