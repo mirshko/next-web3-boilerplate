9d6954cafab1bd2ae7d857e9f3ee95fdbaaa899d
necto68 <necto68necto68@gmail.com>
2023-02-28 14:19:35 +0200

    fix: move RoeAddresses.json to constants

--- hooks/useAddresses.js
+++ hooks/useAddresses.js
@@ -1,5 +1,5 @@
 import { useWeb3React } from "@web3-react/core";
-import addresses from "../contracts/RoeAddresses.json";
+import addresses from "../constants/RoeAddresses.json";
 
 export default function useAddresses(pool) {
   const { chainId } = useWeb3React();
@@ -7,19 +7,23 @@ export default function useAddresses(pool) {
   let chainAddresses;
 
   // deep copy object
-  if (chainId == 1337 ) chainAddresses = JSON.parse(JSON.stringify(addresses["42161"]));
-  else chainAddresses = (addresses[chainId] ? JSON.parse(JSON.stringify(addresses[chainId])) : JSON.parse(JSON.stringify(addresses[1])) );
-  
+  if (chainId == 1337)
+    chainAddresses = JSON.parse(JSON.stringify(addresses["42161"]));
+  else
+    chainAddresses = addresses[chainId]
+      ? JSON.parse(JSON.stringify(addresses[chainId]))
+      : JSON.parse(JSON.stringify(addresses[1]));
+
   // if pool is specified, it acts as a filter for the lendingPools
-  if (pool){
-    let lp = []
-    for (let k = 0; k < chainAddresses['lendingPools'].length; k++){
-      if ( chainAddresses['lendingPools'][k].address == pool ) { 
-        lp = [ chainAddresses['lendingPools'][k] ]; 
+  if (pool) {
+    let lp = [];
+    for (let k = 0; k < chainAddresses["lendingPools"].length; k++) {
+      if (chainAddresses["lendingPools"][k].address == pool) {
+        lp = [chainAddresses["lendingPools"][k]];
         break;
       }
     }
-    chainAddresses['lendingPools'] = lp;
+    chainAddresses["lendingPools"] = lp;
   }
 
   return chainAddresses;
