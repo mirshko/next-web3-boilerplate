671bd21cd637f23ced7e64b944a290675c77d703
Nicolas <lapingarou@gmail.com>
2023-03-05 21:56:05 +0800

    fix: values

--- hooks/useUnderlyingAmount.js
+++ hooks/useUnderlyingAmount.js
@@ -5,7 +5,8 @@ import TR_ABI from "../contracts/TokenizableRange.json"
 import UniswapV2Pair_ABI from "../contracts/UniswapV2Pair.json"
 import { ethers } from "ethers";
 
-export default function useUnderlyingAmount(rangeAddress, vault){
+export default function useUnderlyingAmount(rangeAddress, vault, isTotalUnderlying){
+  // if (isTotalUnderlying): return all underlying, else return for 1e18
   const [tokenAmounts, setTokenAmounts] = useState({amount0: 0, amount1: 0})
   
   var type = 'v3';
@@ -22,17 +23,18 @@ export default function useUnderlyingAmount(rangeAddress, vault){
       var tAmounts, data;
       var oracleValue = await oracle.getAssetPrice(rangeAddress)
       if (type=="v2"){
-        let supplyRatio = 1e18 / ( await ranger.totalSupply() )  // need to balance reserves by token amount to standardize value for 1e18 tokens
+        let supplyRatio = isTotalUnderlying ? 1 : 1e18 / (await ranger.totalSupply()) 
         let res = await ranger.getReserves();
         data = { token0Amount: res[0] * supplyRatio, token1Amount: res[1] * supplyRatio }
       }
       else if (type == "v3" ){
         // changed from getTokenAmounts to getTokenAmountsExcludingFees to ignore potential non compounded fees
-        var data = await ranger.getTokenAmountsExcludingFees(ethers.constants.WeiPerEther)
+        data = await ranger.getTokenAmountsExcludingFees(isTotalUnderlying ? (await ranger.totalSupply()) : ethers.constants.WeiPerEther)
       }
+
       tAmounts = {
-        amount0: data ? ethers.utils.formatUnits(Math.round(data.token0Amount * 1e8 / oracleValue), vault.token0.decimals) : 0,
-        amount1: data ? ethers.utils.formatUnits(Math.round(data.token1Amount * 1e8 / oracleValue), vault.token1.decimals) : 0,
+        amount0: data ? ethers.utils.formatUnits(data.token0Amount, vault.token0.decimals) : 0,
+        amount1: data ? ethers.utils.formatUnits(data.token1Amount, vault.token1.decimals) : 0,
       }
       setTokenAmounts(tAmounts)
     }
