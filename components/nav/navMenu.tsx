1156ccfeec6428c756cdf57fb51dd059ca43cb16
ikura-roe <ikura@roe.finance>
2023-03-21 13:06:00 +0200

    fix: updated staking link

--- components/nav/navMenu.tsx
+++ components/nav/navMenu.tsx
@@ -1,8 +1,15 @@
 // components/layout.js
 import { useState, useEffect } from "react";
-import { Menu } from "antd"
-import { BankOutlined, FrownOutlined, DollarOutlined, DashboardOutlined, SwapOutlined, BugOutlined } from '@ant-design/icons';
-import { useRouter } from 'next/router';
+import { Menu } from "antd";
+import {
+  BankOutlined,
+  FrownOutlined,
+  DollarOutlined,
+  DashboardOutlined,
+  SwapOutlined,
+  BugOutlined,
+} from "@ant-design/icons";
+import { useRouter } from "next/router";
 
 function getItem(label, key, icon, children, type) {
   return {
@@ -14,12 +21,12 @@ function getItem(label, key, icon, children, type) {
   };
 }
 
-const NavMenu = ({bgColor}) =>  {
-  const router = useRouter()
+const NavMenu = ({ bgColor }) => {
+  const router = useRouter();
   const [current, setCurrent] = useState();
-  useEffect ( () => {
-    setCurrent(router.pathname)
-  }, [router.pathname])
+  useEffect(() => {
+    setCurrent(router.pathname);
+  }, [router.pathname]);
 
   const onClick = (e) => {
     router.push(e.key);
@@ -28,25 +35,28 @@ const NavMenu = ({bgColor}) =>  {
   const items = [
     //{ label: 'Vaults', key: '/', icon: <BankOutlined /> },
     //{ label: 'Ranger', key: '/ranger', icon: <BugOutlined /> },
-    { label: 'Protected Perps™', key: '/', icon: <SwapOutlined /> },
-    { label: 'geVault', key: '/gevaults', icon: <BankOutlined /> },
-    { label: 'Dashboard', key: '/dashboard', icon: <DashboardOutlined /> },
+    { label: "Protected Perps™", key: "/", icon: <SwapOutlined /> },
+    { label: "geVault", key: "/gevaults", icon: <BankOutlined /> },
+    { label: "Dashboard", key: "/dashboard", icon: <DashboardOutlined /> },
     //{ label: 'Protected Farming™', key: '/protectedyield', icon: <DollarOutlined /> },
-    { label: 'Staking', key: '/staking', icon: <BankOutlined />, disabled: true },
-  ]
-  
+    {
+      label: "Staking",
+      key: "https://goodentry.io/staking",
+      icon: <BankOutlined />,
+    },
+  ];
+
   return (
     <Menu
       onClick={onClick}
       style={{ backgroundColor: bgColor, borderWidth: 1, minWidth: 500 }}
-      defaultSelectedKeys={['1']}
+      defaultSelectedKeys={["1"]}
       selectedKeys={[current]}
-      defaultOpenKeys={['sub1']}
+      defaultOpenKeys={["sub1"]}
       mode="horizontal"
       items={items}
     />
-  )
-}
-
+  );
+};
 
 export default NavMenu;
