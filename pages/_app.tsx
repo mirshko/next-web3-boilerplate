8b8459d4122bdfff7af2b2de733a39b7eb27d87b
ikura-roe <ikura@roe.finance>
2023-03-28 16:37:22 +0300

    feat: remove password protection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Web3ReactProvider } from "@web3-react/core";
 import type { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
-import { withPasswordProtect } from "next-password-protect";
+// import { withPasswordProtect } from "next-password-protect";
 
 import Layout from "../components/layout";
 import getLibrary from "../getLibrary";
@@ -24,13 +24,15 @@ function NextWeb3App({ Component, pageProps }: AppProps) {
   );
 }
 
-export default process.env.PASSWORD_PROTECT
-  ? withPasswordProtect(NextWeb3App, {
-      loginComponentProps: {
-        backUrl: "https://goodentry.io/",
-        buttonBackgroundColor: "#55D17C",
-        buttonColor: "#ffffff",
-        logo: "/logo.svg",
-      },
-    })
-  : NextWeb3App;
+// export default process.env.PASSWORD_PROTECT
+//   ? withPasswordProtect(NextWeb3App, {
+//       loginComponentProps: {
+//         backUrl: "https://goodentry.io/",
+//         buttonBackgroundColor: "#55D17C",
+//         buttonColor: "#ffffff",
+//         logo: "/logo.svg",
+//       },
+//     })
+//   : NextWeb3App;
+
+export default NextWeb3App;
