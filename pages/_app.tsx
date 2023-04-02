3c4c3924699b2e71d9f84dcbab6c23462a2d2874
caviar-roe <caviar@roe.finance>
2023-04-02 15:15:05 +0200

    feat: OKX wallet integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Web3ReactProvider } from "@web3-react/core";
+import { Web3OnboardProvider } from '@web3-onboard/react'
 import type { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
 // import { withPasswordProtect } from "next-password-protect";
@@ -9,17 +10,20 @@ import { useLogRocketInit } from "../hooks/useLogRocketInit";
 import { appColorScheme } from "../constants/appColorScheme";
 
 import "../styles/globals.css";
+import web3Onboard from "../web3Onboard";
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
   useLogRocketInit();
 
   return (
     <ConfigProvider theme={appColorScheme}>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Web3ReactProvider>
+        <Web3OnboardProvider web3Onboard={web3Onboard}>
+          <Web3ReactProvider getLibrary={getLibrary}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Web3ReactProvider>
+        </Web3OnboardProvider>
     </ConfigProvider>
   );
 }
