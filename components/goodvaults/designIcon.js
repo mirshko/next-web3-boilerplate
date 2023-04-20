fcd5af10a8c9b560fff507d4a9c892c383ffabb6
Nicolas <lapingarou@gmail.com>
2023-04-20 15:05:01 +0800

    feat: icon shadow

--- components/goodvaults/designIcon.js
+++ components/goodvaults/designIcon.js
@@ -4,7 +4,7 @@ const DesignIcon = ({icon, height, alt}) => {
   
   return (
     <div style={{ borderRadius: h/2, border: '1px solid #0FFD6A', width: h, height: h, margin: 'auto', backgroundColor: '#020D0D',
-        display: 'flex', alignItems: 'center', justifyContent: 'center'
+        display: 'flex', alignItems: 'center', justifyContent: 'center', boxShadow: "0px 0px 15px rgba(15, 253, 106, 0.4)"
       }}>
       <img src={icon} height={h / 4} width={h/4} alt={alt} />
     </div>
