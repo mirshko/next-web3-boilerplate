4546e31e8f2c8f10b4a15a245f498e90405ccfed
Nicolas <lapingarou@gmail.com>
2023-05-30 06:35:51 +0800

    style: debt amount

--- components/perps/historyTx.js
+++ components/perps/historyTx.js
@@ -1,6 +1,8 @@
 import ethers from "ethers";
+import useAssetData from "../../hooks/useAssetData";
 
 const HistoryTx = ({tx}) => {
+  const asset = useAssetData(tx.asset)
   const base = tx.ticker ? tx.ticker 
       : tx.strike >= 500 ? "ETH"
         : tx.strike >= 20 ? "GMX"
@@ -18,10 +20,10 @@ const HistoryTx = ({tx}) => {
   return (<tr>
     <td>{base}-USDC</td>
     <td>{action}</td>
-    <td>{sign}{tx.underlying ? parseFloat(tx.underlying.amount0 / 1e18).toFixed(5) : 0} {base}</td>
-    <td>{sign}{tx.underlying ? parseFloat(tx.underlying.amount1 / 1e6).toFixed(3) : 0} USDC</td>
-    <td>0</td>
-    <td>0</td>
+    <td>{tx.underlying ? <>{sign}{parseFloat(tx.underlying.amount0 / 1e18).toFixed(5)}</> : " "} {base}</td>
+    <td>{tx.underlying ? <>{sign}{parseFloat(tx.underlying.amount1 / 1e6).toFixed(3)}</> : " "} USDC</td>
+    <td>{tx.amountDebt ? <>${sign}{parseFloat(tx.amountDebt / 1e18 * asset.oraclePrice).toFixed(3)}</> : " "}</td>
+    <td>-</td>
   </tr>)
 }
 
