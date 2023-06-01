6447cab8205db973ab7165ae71f1621cd027053d
Nicolas <lapingarou@gmail.com>
2023-06-01 18:11:02 +0800

    style: instrument strike

--- components/perps/historyTx.js
+++ components/perps/historyTx.js
@@ -11,7 +11,7 @@ const HistoryTx = ({tx}) => {
   var sign = "-";
   if (tx.type == "BuyOptions") {
     action = "Open";
-    sign = ""
+    sign = "+"
   }
   else if (tx.type == "ClosePosition") action = "Close"
   else if (tx.type == "LiquidatePosition") action = "Liquidation"
@@ -19,7 +19,7 @@ const HistoryTx = ({tx}) => {
      
   return (<tr>
     <td>{new Date(tx.date ?? 0).toLocaleString()}</td>
-    <td>{base}-USDC</td>
+    <td>{base}-{tx.strike}</td>
     <td>{action}</td>
     <td>{tx.underlying ? <>{sign}{parseFloat(tx.underlying.amount0 / 1e18).toFixed(5)}</> : " "} {base}</td>
     <td>{tx.underlying ? <>{sign}{parseFloat(tx.underlying.amount1 / 1e6).toFixed(3)}</> : " "} USDC</td>
