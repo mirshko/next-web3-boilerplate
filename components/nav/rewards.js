4d3abfa634123c5a70bbf35957519501c81e90f6
Nicolas <lapingarou@gmail.com>
2023-06-23 20:35:23 +0800

    feat: GOOD rewards in nav

--- components/nav/rewards.js
+++ components/nav/rewards.js
@@ -1,26 +1,41 @@
-import { Button } from "antd";
-
+import { useState } from "react";
+import { Button, Modal } from "antd";
+import useRewards from "../../hooks/useRewards";
 
 const Rewards = ({}) => {
-  
+  const rewards = useRewards();
+  console.log(rewards)
+  const [visible, setVisible] = useState();
   
   return (
-  <Button
-    style={{
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      marginRight: 8,
-      gap: 8
-    }}
-    icon=<img src="/icons/good.svg" 
-          height={16}
-          width={16}
-          alt="Good Token" />
-    onClick={()=>{}}
-  >
-    0.0
-  </Button>
+  <>
+    <Button
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        marginRight: 8,
+        gap: 8
+      }}
+      icon=<img src="/icons/good.svg" 
+            height={16}
+            width={16}
+            alt="Good Token" />
+      onClick={()=>{}}
+    >
+      {rewards.user_good ? (rewards.user_good).toFixed(2) : 0.0 }
+    </Button>
+    <Modal 
+      title="GOOD Rewards"
+      open={visible} onOk={()=>{setVisible(false)}} onCancel={()=>{setVisible(false)}}
+      footer={null}
+      >
+      50M $GOOD rewards will be airdropped after a 4 months liquidity mining program.
+      <br />
+      <br />
+      Breakdown: 
+    </Modal>
+  </>
   )
 }
 
--- hooks/useRewards.js
+++ hooks/useRewards.js
@@ -1,29 +1,42 @@
 import { useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core";
 import axios from 'axios'; 
 
 const REWARDS_URL = "https://roe.nicodeva.xyz/stats/arbitrum/total_rewards.json";
 
-export default function useRewards(address) {
+export default function useRewards() {
+  const { account } = useWeb3React();
   const [rewards, setRewards] = useState({})
   
   useEffect(()=>{
     const getData = async () => {
       try {
         const rdata = (await axios.get(REWARDS_URL)).data;
-        if (rdata.hasOwnProperty(address)){
-          setRewards({
-            rounds: rdata.rounds,
-            totalDebt: rdata[address].totalDebt,
-            totalGev: rdata[address].totalGev,
-          })
+        let rew = {
+          good_per_round: 17361 // 5% of 1B supply as airdrop, so 50M / 120d / 24 rounds  = 17361 / round
         }
+        if (rdata.rounds) rew.rounds = rdata.rounds;
+        if (rdata.points) {
+          rew.points = rdata.points;
+          rew.distributed = parseFloat(rdata.points) * rew.good_per_round;
+        }
+        if (rdata.hasOwnProperty(account)){
+          rew = {
+            ...rew, 
+            user_points: rdata[account].points,
+            user_good: parseFloat(rdata[account].points) * rew.good_per_round,
+            totalDebt: rdata[account].totalDebt,
+            totalGev: rdata[account].totalGev,
+          }
+        }
+        setRewards(rew)
       }
       catch(e) {
         console.log("Get Rewards", e);
       }
     }
     getData()
-  }, [address])
+  }, [account])
   
-  return data;
+  return rewards;
 }
